fe90659cc84da2da637182f54721e864a502e7a7
Rick Royd Aban <Velosi@Velosis-MacBook-Air.local>
2015-05-18 17:56:37 +0800

    Initial Commit

---
+++ Salt iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Salt iOS
+//
+//  Created by Rick Royd Aban on 5/18/15.
+//  Copyright (c) 2015 Rick Royd Aban. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Salt iOSTests/Salt_iOSTests.m
@@ -0,0 +1,40 @@
+//
+//  Salt_iOSTests.m
+//  Salt iOSTests
+//
+//  Created by Rick Royd Aban on 5/18/15.
+//  Copyright (c) 2015 Rick Royd Aban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Salt_iOSTests : XCTestCase
+
+@end
+
+@implementation Salt_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
