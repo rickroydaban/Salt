dfabd9b82ddc97803067caee64aef239f43a3f5b
Rick Royd Aban <Velosi@Velosis-MacBook-Air.local>
2015-07-20 16:47:31 +0800

    teet

---
+++ Salt iOS/VCHomeScrollPage.h
@@ -0,0 +1,16 @@
+//
+//  VCHomeScrollPage.h
+//  Salt iOS
+//
+//  Created by Rick Royd Aban on 5/20/15.
+//  Copyright (c) 2015 Rick Royd Aban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@interface VCHomeScrollPage : UITableViewController
+
+@property (strong, nonatomic) AppDelegate *propAppDelegate;
+
+@end
---
+++ Salt iOS/VCHomeScrollPage.m
@@ -0,0 +1,23 @@
+//
+//  VCHomeScrollPage.m
+//  Salt iOS
+//
+//  Created by Rick Royd Aban on 5/20/15.
+//  Copyright (c) 2015 Rick Royd Aban. All rights reserved.
+//
+
+#import "VCHomeScrollPage.h"
+
+@interface VCHomeScrollPage ()
+
+@end
+
+@implementation VCHomeScrollPage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    _propAppDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+}
+
+@end
